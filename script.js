ae21298723c11d6ed68a3de1223f21e88feb326e
Cooberthy <lebea012@osloskolen.no>
2026-09-08 09:44:02 +0200

    spørsmål display ferdig

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-let list = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "so carefull of the type? but no from scarped cliff and quarried stone se cries a thousands types are fona i care for nothing all shall go", "never gonna give you up never gonna let you down never gonna run around and desert you", "God said, Let there be light: and there was light.", "dette sitatet er sponset av Raid: Shadow Legends™", "this is pointless"]
-let word = list[Math.floor(Math.random() * list.length)]
+let list = ["hva står css for",]
+let answer1 = ["crumbling style sheet"]
+let answer2 = ["cascading style sheet"]
+let answer3 = ["creaking style shark"]
+let answer4 = ["cmen sloving smen"]
 
 function wow(){
     test = Math.floor(Math.random() * list.length)
-    word = list[test]
-    document.getElementById("question").textContent = word
-    console.log(word)
+    question = list[test]
+    ans1 = answer1[test]
+    ans2 = answer2[test]
+    ans3 = answer3[test]
+    ans4 = answer4[test]
+    document.getElementById("question").textContent = question
+    document.getElementById("ans1").textContent = ans1
+    document.getElementById("ans2").textContent = ans2
+    document.getElementById("ans3").textContent = ans3
+    document.getElementById("ans4").textContent = ans4
 }
