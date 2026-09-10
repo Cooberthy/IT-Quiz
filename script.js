b14c86c06dcf5c6161186ad148425d3db96fd92b
Cooberthy <lebea012@osloskolen.no>
2026-09-10 14:04:31 +0200

    lage spærsmål

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-let list = ["hva står css for", "hva står RAM for",]
-let answer1 = ["crumbling style sheet", "random access memory",]
-let answer2 = ["cascading style sheet", "respotory aligning mass"]
-let answer3 = ["creaking style shark", "repeated acquiring method"]
-let answer4 = ["cmen sloving smen", "rvolving aspire melonly"]
+let list = ["hva står css for", "hva står RAM for", "hvilken wifi kanal skal du ikke bruke"]
+let answer1 = ["crumbling style sheet", "random access memory", "11"]
+let answer2 = ["cascading style sheet", "respotory aligning mass", "4"]
+let answer3 = ["creaking style shark", "repeated acquiring method", "14"]
+let answer4 = ["cmen sloving smen", "revolving aspire melonly", "6"]
 let right = ["2", "1"]
 let poeng = 0
+let repeat = []
+
 
 function wow(){
     test = Math.floor(Math.random() * list.length)
+    if(repeat.includes(test) == true){
+        console.log("peak")
+    }
+    while(repeat.includes(test)){
+        test = Math.floor(Math.random() * list.length)   
+    }
     question = list[test]
     ans1 = answer1[test]
     ans2 = answer2[test]
@@ -19,6 +27,7 @@ function wow(){
     document.getElementById("ans2").textContent = ans2
     document.getElementById("ans3").textContent = ans3
     document.getElementById("ans4").textContent = ans4
+    repeat.push(test)
 }
 
 function check1(){
