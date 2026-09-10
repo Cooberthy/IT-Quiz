5f01d6b2c638434c1aa1bacba98b928c270b4d58
Cooberthy <lebea012@osloskolen.no>
2026-09-10 14:10:36 +0200

    sprøsmål 5

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let list = ["hva står css for", "hva står RAM for", "hvilken wifi kanal skal du ikke bruke"]
-let answer1 = ["crumbling style sheet", "random access memory", "11"]
-let answer2 = ["cascading style sheet", "respotory aligning mass", "4"]
-let answer3 = ["creaking style shark", "repeated acquiring method", "14"]
-let answer4 = ["cmen sloving smen", "revolving aspire melonly", "6"]
-let right = ["2", "1"]
+let list = ["hva står css for", "hva står RAM for", "hvilken wifi kanal skal du ikke bruke", "hvilket gdpr prinsipp er riktighet",
+    "hva er det beste IT tipse"
+]
+let answer1 = ["crumbling style sheet", "random access memory", "11", "1", "har du prøvd å skru den av og på"]
+let answer2 = ["cascading style sheet", "respotory aligning mass", "4", "3", "har du spørt osloskolenGPT"]
+let answer3 = ["creaking style shark", "repeated acquiring method", "14", "7", "git gud"]
+let answer4 = ["cmen sloving smen", "revolving aspire melonly", "6", "4", "gi opp"]
+let right = ["2", "1", "3", "4", "1"]
 let poeng = 0
 let repeat = []
+let answered = 0
 
 
 function wow(){
@@ -28,6 +31,7 @@ function wow(){
     document.getElementById("ans3").textContent = ans3
     document.getElementById("ans4").textContent = ans4
     repeat.push(test)
+    answered+= 1
 }
 
 function check1(){
